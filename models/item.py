d7de089824f49836537e0635c0d015e0bcdfe585
Kim Nybo Andersen (Tzulia) <tzulia@kimna.dk>
2019-02-17 12:56:47 +0100

    Added code for making item names and store names unique in the DB eyes.
    This closes #3

--- models/item.py
+++ models/item.py
@@ -5,7 +5,7 @@ class ItemModel(db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False, unique=True)
     price = db.Column(db.Float(precision=2), nullable=False)
 
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
